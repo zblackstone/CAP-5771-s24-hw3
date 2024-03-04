736fffc668cab5690096df65ce60f62ec9cc27e5
zblackstone <144168057+zblackstone@users.noreply.github.com>
2024-03-04 18:37:59 -0500

    Initial commit

---
+++ INSTRUCTIONS.md
@@ -0,0 +1,16 @@
+# Homework 3
+- The entire assignment is done in the file `all_questions.py`. 
+- Do not change the name of the file. 
+- You can run python on this file to check for compilation errors. Do so BEFORE uploading to Gradescope. 
+```code
+    python all_questions.py
+```
+- Each question is answered using a dictionary that is set up in the various functions in the file `all_questions.py`
+- There are 10 questions. 
+- Some answers require explanations, which you should add to your answers as a string. 
+- Until the due date, you can submit to gradescope, which will do structural checks
+    - make sure there are no missing keys
+    - check that the answer types (float/string) are correct
+- The answers themselves, which are graded, are checked after the due date has passed. 
+
+### Report any errors or questions on the discussion area on Canvas for Homework 3.
---
+++ all_questions.py
@@ -0,0 +1,382 @@
+# Add import files
+import pickle
+
+
+
+# -----------------------------------------------------------
+def question1():
+    answers = {}
+
+    # type: bool (True/False)
+    answers["(a)"] = -1
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain"] = ""
+
+    # type: bool (True/False)
+    answers["(b)"] = -1
+
+    # type: explanatory string (at least four words)
+    answers["(b) explain"] = ""
+
+    # type: bool (True/False)
+    answers["(c)"] = -1
+
+    # type: explanatory string (at least four words)
+    answers["(c) explain"] = ""
+
+    # type: bool (True/False)
+    answers["(d)"] = -1
+
+    # type: explanatory string (at least four words)
+    answers["(d) explain"] = ""
+
+    # type: bool (True/False)
+    answers["(e)"] = -1
+
+    # type: explanatory string (at least four words)
+    answers["(e) explain"] = ""
+
+    # type: bool (True/False)
+    answers["(f)"] = -1
+
+    # type: explanatory string (at least four words)
+    answers["(f) explain"] = ""
+
+    # type: bool (True/False)
+    answers["(g)"] = -1
+
+    # type: explanatory string (at least four words)
+    answers["(g) explain"] = ""
+
+    # type: bool (True/False)
+    answers["(h)"] = -1
+
+    # type: explanatory string (at least four words)
+    answers["(h) explain"] = ""
+
+    # type: bool (True/False)
+    answers["(i)"] = -1
+
+    # type: explanatory string (at least four words)
+    answers["(i) explain"] = ""
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question2():
+    answers = {}
+
+    # type: bool (True/False)
+    answers["(a)"] = -1
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain"] = ""
+
+    # type: bool (True/False)
+    answers["(b)"] = -1
+
+    # type: explanatory string (at least four words)
+    answers["(b) explain"] = ""
+
+    # type: bool (True/False)
+    answers["(c)"] = -1
+
+    # type: explanatory string (at least four words)
+    answers["(c) explain"] = ""
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question3():
+    answers = {}
+
+    # type: a string that evaluates to a float
+    answers["(a) SSE"] = 7.5
+
+    # type: a string that evaluates to a float
+    answers["(b) SSE"] = 7.5
+
+    # type: a string that evaluates to a float
+    answers["(c) SSE"] = 7.5
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question4():
+    answers = {}
+
+    # type: int
+    answers["(a) Circle (a)"] = 0
+
+    # type: int
+    answers["(a) Circle (b)"] = 0
+
+    # type: int
+    answers["(a) Circle (c)"] = 0
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain"] = ""
+
+    # type: int
+    answers["(b) Circle (a)"] = 0
+
+    # type: int
+    answers["(b) Circle (b)"] = 0
+
+    # type: int
+    answers["(b) Circle (c)"] = 0
+
+    # type: explanatory string (at least four words)
+    answers["(b) explain"] = ""
+
+    # type: int
+    answers["(c) Circle (a)"] = 0
+
+    # type: int
+    answers["(c) Circle (b)"] = 0
+
+    # type: int
+    answers["(c) Circle (c)"] = 0
+
+    # type: explanatory string (at least four words)
+    answers["(c) explain"] = ""
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question5():
+    answers = {}
+
+    # type: set
+    answers["(a)"] = set()
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain"] = ""
+
+    # type: set
+    answers["(b)"] = set()
+
+    # type: explanatory string (at least four words)
+    answers["(b) explain"] = ""
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question6():
+    answers = {}
+
+    # type: set
+    answers["(a) core"] = set()
+
+    # type: set
+    answers["(a) boundary"] = set()
+
+    # type: set
+    answers["(a) noise"] = set()
+
+    # type: set
+    answers["(b) cluster 1"] = set()
+
+    # type: set
+    answers["(b) cluster 2"] = set()
+
+    # type: set
+    answers["(b) cluster 3"] = set()
+
+    # type: set
+    answers["(b) cluster 4"] = set()
+
+    # type: set
+    answers["(c)-a core"] = set()
+
+    # type: set
+    answers["(c)-a boundary"] = set()
+
+    # type: set
+    answers["(c)-a noise"] = set()
+
+    # type: set
+    answers["(c)-b cluster 1"] = set()
+
+    # type: set
+    answers["(c)-b cluster 2"] = set()
+
+    # type: set
+    answers["(c)-b cluster 3"] = set()
+
+    # type: set
+    answers["(c)-b cluster 4"] = set()
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question7():
+    answers = {}
+
+    # type: string
+    answers["(a)"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain"] = ""
+
+    # type: string
+    answers["(b)"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(b) explain"] = ""
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question8():
+    answers = {}
+
+    # type: string
+    answers["(a) Matrix 1"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain diag entries, Matrix 1"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain non-diag entries, Matrix 1"] = ""
+
+    # type: string
+    answers["(a) Matrix 2"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain diag entries, Matrix 2"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain non-diag entries, Matrix 2"] = ""
+
+    # type: string
+    answers["(a) Matrix 3"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain diag entries, Matrix 3"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain non-diag entries, Matrix 3"] = ""
+
+    # type: string
+    answers["(b) Row 1"] = ""
+
+    # type: string
+    answers["(b) Row 2"] = ""
+
+    # type: string
+    answers["(b) Row 3"] = ""
+
+    # type: string
+    answers["(b) Row 4"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(b) Row 1 explain"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(b) Row 2 explain"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(b) Row 3 explain"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(b) Row 4 explain"] = ""
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question9():
+    answers = {}
+
+    # type: list
+    answers["(a)"] = []
+
+    # type: list
+    answers["(b)"] = []
+
+    # type: list
+    answers["(c)"] = []
+
+    # type: list
+    answers["(d)"] = []
+
+    # type: list
+    answers["(e)"] = []
+
+    # type: explanatory string (at least four words)
+    answers["(e) explain"] = ""
+
+    return answers
+
+
+
+
+# -----------------------------------------------------------
+def question10():
+    answers = {}
+
+    # type: string
+    answers["(a) Figure (a)"] = ""
+
+    # type: string
+    answers["(a) Figure (b)"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(a) explain"] = ""
+
+    # type: string
+    answers["(b) Figure (a)"] = ""
+
+    # type: string
+    answers["(b) Figure (b)"] = ""
+
+    # type: explanatory string (at least four words)
+    answers["(b) explain"] = ""
+
+    # type: string
+    answers["(c)"] = ""
+
+    return answers
+
+# --------------------------------------------------------
+if __name__ == '__main__':
+    answers_dict = {}
+    answers_dict['question1'] = question1()
+    answers_dict['question2'] = question2()
+    answers_dict['question3'] = question3()
+    answers_dict['question4'] = question4()
+    answers_dict['question5'] = question5()
+    answers_dict['question6'] = question6()
+    answers_dict['question7'] = question7()
+    answers_dict['question8'] = question8()
+    answers_dict['question9'] = question9()
+    answers_dict['question10'] = question10()
+    print('end code')
+
+    with open('answers.pkl', 'wb') as f:
+        pickle.dump(answers_dict, f)
